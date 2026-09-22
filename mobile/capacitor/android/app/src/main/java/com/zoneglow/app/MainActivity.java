c5ea7944af5454bd05f24439b2018219622383dc
rygodir95 <balazs.szemak.95@gmail.com>
2026-09-22 17:15:17 +0200

    Add Hub BLE client support: mobile/capacitor/android/app/src/main/java/com/zoneglow/app/MainActivity.java

--- mobile/capacitor/android/app/src/main/java/com/zoneglow/app/MainActivity.java
+++ mobile/capacitor/android/app/src/main/java/com/zoneglow/app/MainActivity.java
@@ -2,4 +2,11 @@ package com.zoneglow.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(android.os.Bundle savedInstanceState) {
+        registerPlugin(HubBlePlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
+
